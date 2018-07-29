2972b7eec04174c758a78d4334c8522f73e5f8be
ihsan-96 <ameenahsanma@gmail.com>
2018-07-29 16:42:53 +0530

    Revert "heroku"
    This reverts commit c100c513cde8c72e26052c7381fdb2f634edecf5.

--- tcsgo/settings.py
+++ tcsgo/settings.py
@@ -71,18 +71,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tcsgo.wsgi.application'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-import dj_database_url
-
-DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tcsgo_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -120,14 +122,8 @@ LOGIN_REDIRECT_URL = 'rides:groups'
 
 # LOGIN_REDIRECT_URL = 'rides:groups'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
